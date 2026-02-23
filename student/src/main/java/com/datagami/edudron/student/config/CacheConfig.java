96c9f9379edf3fa1f9f2332d6dfa682c3b61b682
kunal sharma <kunalsharma.ak23@gmail.com>
2026-02-23 15:00:10 +0530

    feat: Add `studentCourseAnalytics` to the list of managed Caffeine caches.

--- student/src/main/java/com/datagami/edudron/student/config/CacheConfig.java
+++ student/src/main/java/com/datagami/edudron/student/config/CacheConfig.java
@@ -16,18 +16,18 @@ public class CacheConfig {
     @Bean
     public CacheManager cacheManager() {
         CaffeineCacheManager cacheManager = new CaffeineCacheManager(
-            "courseAnalytics",
-            "lectureAnalytics",
-            "sectionAnalytics",
-            "classAnalytics",
-            "examFromContent"      // Cached exam from Content service (1 min TTL, 2000 max for scale)
+                "courseAnalytics",
+                "lectureAnalytics",
+                "sectionAnalytics",
+                "classAnalytics",
+                "studentCourseAnalytics",
+                "examFromContent" // Cached exam from Content service (1 min TTL, 2000 max for scale)
         );
         cacheManager.setCaffeine(
-            Caffeine.newBuilder()
-                .maximumSize(2000)
-                .expireAfterWrite(1, TimeUnit.MINUTES)
-                .recordStats()
-        );
+                Caffeine.newBuilder()
+                        .maximumSize(2000)
+                        .expireAfterWrite(1, TimeUnit.MINUTES)
+                        .recordStats());
         return cacheManager;
     }
 }
